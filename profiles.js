19bd8cb1f2df9650023d9bb013172d0aebf63b9a
PriHL <prhorita.arquitetura@hotmail.com>
2025-05-23 14:30:04 -0300

    Update profiles.js

--- profiles.js
+++ profiles.js
@@ -1,12 +1,12 @@
 const profiles = [
-  { id: 1, username: "perfil_principal1", status: "ativo", grupo: "A" },
-  { id: 2, username: "perfil_principal2", status: "ativo", grupo: "A" },
-  { id: 3, username: "perfil_principal3", status: "pausado", grupo: "A" },
-  { id: 4, username: "perfil_principal4", status: "ativo", grupo: "A" },
-  { id: 5, username: "perfil_principal5", status: "ativo", grupo: "A" },
-  { id: 6, username: "perfil_principal6", status: "ativo", grupo: "A" },
-  { id: 7, username: "perfil_principal7", status: "ativo", grupo: "A" },
-  { id: 8, username: "perfil_principal8", status: "ativo", grupo: "A" },
+  { id: 1, username: "2m.scouting", status: "ativo", grupo: "A" },
+  { id: 2, username: "profissional.dm", status: "ativo", grupo: "A" },
+  { id: 3, username: "scou.mmodels", status: "pausado", grupo: "A" },
+  { id: 4, username: "scouteronline21", status: "ativo", grupo: "A" },
+  { id: 5, username: "virtual.scoutt", status: "ativo", grupo: "A" },
+  { id: 6, username: "on.scouter", status: "ativo", grupo: "A" },
+  { id: 7, username: "mood.profissional", status: "ativo", grupo: "A" },
+  { id: 8, username: "profissional.scout", status: "ativo", grupo: "A" },
 
   { id: 9, username: "perfil_secundario1", status: "ativo", grupo: "B" },
   { id: 10, username: "perfil_secundario2", status: "ativo", grupo: "B" }
